bc61ede4366ed5741aad7e65d4a4b5075df2db7d
= <=>
2023-09-10 14:55:56 -0500

    more updates to the card

--- src/pages/FriendTechUserDetail.js
+++ src/pages/FriendTechUserDetail.js
@@ -33,8 +33,6 @@ function FriendTechUserDetail(){
     
         return `${baseClasses} ${hoverOrSelectedClasses}`;
     };
-    
-
 
     return (
         <div className={`flex items-start justify-between h-screen px-6 py-4 ${theme === 'light' ? 'bg-gray-100' : 'bg-dark-primary'}`}>
@@ -56,25 +54,58 @@ function FriendTechUserDetail(){
                     </div>
                 </div>
 
-                <div className="max-w-md w-full bg-white dark:bg-dark-secondary rounded shadow p-4 space-y-6 overflow-y-auto">
-                    <div className="flex items-center justify-center border-b dark:border-gray-700 pb-4">
-                        <img 
-                            src={userData.twitter_profile_pic} 
-                            alt="User Profile" 
-                            className="w-12 h-12 rounded-full object-cover mr-3"
-                        />
-                        <div>
-                            <h1 className="text-xl text-gray-800 dark:text-light-foreground mb-1">{userData.twitter_username}</h1>
-                            <p className="text-gray-500 dark:text-gray-400">Address: {userData.address}</p>
-                            <p className="text-gray-500 dark:text-gray-400">ShareSupply: {userData.shares_supply}</p>
-                            <p className="text-gray-500 dark:text-gray-400">Followers: {userData.twitter_followers ? userData.twitter_followers.toLocaleString() : null}</p>
+                {userData && (
+                    <div className="max-w-md w-full bg-white dark:bg-dark-secondary rounded-lg shadow-md overflow-hidden">
+                        {userData.twitter_profile_banner && (
+                            <div 
+                                className="w-full h-24 bg-cover bg-center" 
+                                style={{ backgroundImage: `url(${userData.twitter_profile_banner})` }}
+                            ></div>
+                        )}
+                        <div className="p-4 space-y-6">
+                            <div className="flex items-center justify-between">
+                                <img 
+                                    src={userData.twitter_profile_pic} 
+                                    alt="User Profile" 
+                                    className="w-16 h-16 rounded-full object-cover shadow-lg"
+                                />
+                                <div className="flex-1 ml-4">
+                                    <h1 className="text-2xl text-gray-800 dark:text-light-foreground font-semibold mb-2">{userData.twitter_username}</h1>
+                                    {
+                                        userData.address && (
+                                            <p className="text-gray-500 dark:text-gray-400">
+                                                Address: {`${userData.address.slice(0, 6)}...${userData.address.slice(-4)}`}
+                                            </p>
+                                        )
+                                    }
+                                    
+                                    <p className="text-gray-500 dark:text-gray-400">ShareSupply: {userData.shares_supply}</p>
+                                    <p className="text-gray-500 dark:text-gray-400">Followers: {userData.twitter_followers ? userData.twitter_followers.toLocaleString() : null}</p>
+                                </div>
+                                {userData.verified && 
+                                    <div className="text-xs bg-blue-500 text-white rounded-full px-2 py-1 self-start">Verified</div>
+                                }
+                            </div>
+
+                            <div>
+                                <p className="text-gray-700 dark:text-gray-300 font-medium">
+                                    Current price: Ξ{userData.candle_stick_data?.length > 0 ? userData.candle_stick_data[userData.candle_stick_data.length-1].low : null}
+                                </p>
+                                <p className="text-gray-500 dark:text-gray-400">
+                                    Holders: {userData.holder_count ? userData.holder_count.toLocaleString() : 0}
+                                </p>
+                                <p className="text-gray-500 dark:text-gray-400">
+                                    Holdings: {userData.holding_count ? userData.holding_count.toLocaleString() : 0}
+                                </p>
+                                <p className="text-gray-500 dark:text-gray-400">
+                                    Last Online: {new Date(userData.last_online).toLocaleString()}
+                                </p>
+                            </div>
                         </div>
                     </div>
+                )}
 
-                    <p className="text-gray-600 dark:text-light-foreground">
-                        Current price {userData.candle_stick_data.length > 0 ? userData.candle_stick_data[userData.candle_stick_data.length-1].low : null}
-                    </p>
-                </div>
+
         </div>
     )
 }
