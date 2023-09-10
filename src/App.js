442c279138acdee0274afa744d7cd6dff662a174
= <=>
2023-09-09 20:12:34 -0500

    update js and dashboard

--- src/App.js
+++ src/App.js
@@ -1,114 +1,73 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { createChart } from 'lightweight-charts';
-import { RSI } from 'technicalindicators';
-import axios from 'axios';
-import "./test.css"
+import React from 'react';
+
+import useCandleStickData from './hooks/usecandlestickdata';
+
 
 function App() {
-    const chartContainerRef = useRef(null);
-
-    const [ userData, setUserData ] = useState({candle_stick_data: []})
-    const [ searchState, setSearchState ] = useState(null)
-    const [ interval, setInterval ] = useState(300)
-
-    const handleDropdownChange = (e) => {
-      setInterval(e.target.value);
-  }
-
-    async function getData(){
-      const url = `http://localhost:8000/friend-trader/friend-tech-users/${searchState}/?interval=${interval}`;
-      try {
-          const userDataRes = await axios.get(url);
-          console.log(userDataRes)
-          setUserData(userDataRes.data);
-        } catch (e) {
-            console.log(e);
-        }
-    }
     
-    useEffect(() => {
-      const chart = createChart(chartContainerRef.current, {
-        width: window.innerWidth * .65,
-        height: window.innerHeight * 0.7, 
-          timeScale: {
-              rightOffset: 50,
-              barSpacing: 3,
-              // fixLeftEdge: true,
-          },
-          layout: {
-              backgroundColor: '#ffffff',
-              textColor: '#333',
-          },
-          grid: {
-              vertLines: {
-                  color: '#efefef',
-              },
-              horzLines: {
-                  color: '#efefef',
-              },
-          },
-      });
-  
-        if (userData.candle_stick_data.length > 0) {
-            
-            const candlestickSeries = chart.addCandlestickSeries();
-            candlestickSeries.setData(userData.candle_stick_data);
-        }
-        
-        return () => chart.remove();
-    }, [userData.candle_stick_data]);
+    const {
+        chartContainerRef,
+        rsiContainerRef,
+        userData,
+        getData,
+        setSearchState,
+        handleDropdownChange
+    } = useCandleStickData()
 
     return (
-<div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh", backgroundColor: "#f5f5f5", position: "relative" }}>
-
-    
-
-    <div style={{ maxWidth: "500px", width: "100%", backgroundColor: "#ffffff", borderRadius: "8px", padding: "16px", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)", zIndex: 10, marginBottom: "10px" }}>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", borderBottom: "1px solid #e0e0e0", paddingBottom: "16px" }}>
-            <img 
-                src={userData.twitter_profile_pic} 
-                alt="User Profile" 
-                style={{ width: "50px", height: "50px", borderRadius: "50%", objectFit: "cover", marginRight: "8px" }}
-            />
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between",  backgroundColor: "#f5f5f5", height: "100vh"}}>
             <div>
-                <h1 style={{ margin: 0, fontSize: "20px", color: "#333" }}>{userData.twitter_username}</h1>
-                <p style={{ margin: "4px 0 0 0", fontSize: "14px", color: "#888" }}>ShareSupply: {userData.shares_supply}</p>
-                <p style={{ margin: "4px 0 0 0", fontSize: "14px", color: "#888" }}>Followers: {userData.twitter_followers ? userData.twitter_followers.toLocaleString() : null}</p>
+                <div ref={chartContainerRef}>
+                    {/* Chart will be rendered here */}
+                </div>
+                <div ref={rsiContainerRef} style={{ height: '20%' }}>
+                    {/* RSI will be rendered here */}
+                </div>
+            </div>
+            <div>
+                <div style={{ maxWidth: "500px", width: "100%", backgroundColor: "#ffffff", borderRadius: "8px", padding: "16px", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)", zIndex: 10, marginBottom: "10px" }}>
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", borderBottom: "1px solid #e0e0e0", paddingBottom: "16px" }}>
+                        <img 
+                            src={userData.twitter_profile_pic} 
+                            alt="User Profile" 
+                            style={{ width: "50px", height: "50px", borderRadius: "50%", objectFit: "cover", marginRight: "8px" }}
+                        />
+                        <div>
+                            <h1 style={{ margin: 0, fontSize: "20px", color: "#333" }}>{userData.twitter_username}</h1>
+                            <p style={{ margin: "4px 0 0 0", fontSize: "14px", color: "#888" }}>ShareSupply: {userData.shares_supply}</p>
+                            <p style={{ margin: "4px 0 0 0", fontSize: "14px", color: "#888" }}>Followers: {userData.twitter_followers ? userData.twitter_followers.toLocaleString() : null}</p>
+                        </div>
+                    </div>
+
+                    <div style={{ display: "flex", alignItems: "center", marginTop: "16px" }}>
+                        <select 
+                            style={{ flex: 1, marginRight: "8px", padding: "4px 8px", borderRadius: "4px", border: "1px solid #e0e0e0", fontSize: "14px" }} 
+                            onChange={handleDropdownChange}
+                        >
+                            <option value="300">5 minutes</option>
+                            <option value="3600">1 hour</option>
+                            <option value="14400">4 hours</option>
+                        </select>
+
+                        <input 
+                            style={{ flex: 2, marginRight: "8px", padding: "4px 8px", borderRadius: "4px", border: "1px solid #e0e0e0", fontSize: "14px" }}
+                            onChange={(e) => setSearchState(e.target.value)}
+                        />
+
+                        <button 
+                            style={{ padding: "4px 16px", backgroundColor: "#007BFF", color: "#fff", borderRadius: "4px", border: "none", cursor: "pointer" }}
+                            onClick={getData}
+                        >
+                            Search
+                        </button>
+                    </div>
+
+                    <p style={{ marginTop: "12px", fontSize: "14px", color: "#444" }}>
+                        Current price {userData.candle_stick_data.length > 0 ? userData.candle_stick_data[userData.candle_stick_data.length-1].low : null}
+                    </p>
+                </div>
             </div>
         </div>
-
-        <div style={{ display: "flex", alignItems: "center", marginTop: "16px" }}>
-            <select 
-                style={{ flex: 1, marginRight: "8px", padding: "4px 8px", borderRadius: "4px", border: "1px solid #e0e0e0", fontSize: "14px" }} 
-                onChange={handleDropdownChange}
-            >
-                <option value="300">5 minutes</option>
-                <option value="3600">1 hour</option>
-                <option value="14400">4 hours</option>
-            </select>
-
-            <input 
-                style={{ flex: 2, marginRight: "8px", padding: "4px 8px", borderRadius: "4px", border: "1px solid #e0e0e0", fontSize: "14px" }}
-                onChange={(e) => setSearchState(e.target.value)}
-            />
-
-            <button 
-                style={{ padding: "4px 16px", backgroundColor: "#007BFF", color: "#fff", borderRadius: "4px", border: "none", cursor: "pointer" }}
-                onClick={getData}
-            >
-                Search
-            </button>
-        </div>
-
-        <p style={{ marginTop: "12px", fontSize: "14px", color: "#444" }}>
-            Current price {userData.candle_stick_data.length > 0 ? userData.candle_stick_data[userData.candle_stick_data.length-1].low : null}
-        </p>
-    </div>
-
-    <div ref={chartContainerRef}>
-        {/* Chart will be rendered here */}
-    </div>
-</div>
     )
 }
 
---
+++ src/hooks/usecandlestickdata.js
@@ -0,0 +1,143 @@
+import { useState, useRef, useEffect } from "react";
+import { createChart } from 'lightweight-charts';
+import { RSI } from 'technicalindicators';
+import axios from 'axios';
+
+
+export default function useCandleStickData(){
+    const chartContainerRef = useRef(null);
+    const rsiContainerRef = useRef(null);
+    const [ userData, setUserData ] = useState({candle_stick_data: []})
+    const [ searchState, setSearchState ] = useState(null)
+    const [ interval, setInterval ] = useState(300)
+
+    const handleDropdownChange = (e) => {
+      setInterval(e.target.value);
+  }
+
+    async function getData(){
+      const url = `http://localhost:8000/friend-trader/friend-tech-users/${searchState}/?interval=${interval}`;
+      try {
+          const userDataRes = await axios.get(url);
+          console.log(userDataRes)
+          setUserData(userDataRes.data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    
+    useEffect(() => {
+      const chart = createChart(chartContainerRef.current, {
+        width: window.innerWidth * .65,
+        height: window.innerHeight * 0.6, 
+          timeScale: {
+              rightOffset: 50,
+              barSpacing: 3,
+              // fixLeftEdge: true,
+          },
+          layout: {
+              backgroundColor: '#ffffff',
+              textColor: '#333',
+          },
+          grid: {
+              vertLines: {
+                  color: '#efefef',
+              },
+              horzLines: {
+                  color: '#efefef',
+              },
+          },
+      });
+
+      const rsiChart = createChart(rsiContainerRef.current, {
+        width: window.innerWidth * .65,
+        height: window.innerHeight * 0.14,  // Taking 20% of 0.7 which was originally used
+        timeScale: {
+            rightOffset: 50,
+            barSpacing: 3,
+        },
+        layout: {
+            backgroundColor: '#ffffff',
+            textColor: '#333',
+        },
+        grid: {
+            vertLines: {
+                color: '#efefef',
+            },
+            horzLines: {
+                color: '#efefef',
+            },
+        },
+        priceScale: {
+            autoScale: true,
+            borderColor: '#555',
+        },
+        timeScale: {
+            borderColor: '#555',
+        },
+        localization: {
+            priceFormatter: function(price) {
+                // This ensures that the RSI value is shown as a plain number without extra decimals
+                return parseFloat(price).toFixed(2);
+            },
+        },
+    });
+
+      if (userData.candle_stick_data.length > 0) {
+            const candlestickSeries = chart.addCandlestickSeries();
+            candlestickSeries.setData(userData.candle_stick_data);
+
+            // Calculate and display the RSI
+            const closePrices = userData.candle_stick_data.map(candle => candle.close);
+            const rsiValues = RSI.calculate({ values: closePrices, period: 14 });
+            const rsiSeries = rsiChart.addLineSeries({
+                color: 'black',
+                lineWidth: 2,
+            });
+            rsiSeries.setData(
+                userData.candle_stick_data.slice(14).map((candle, index) => ({
+                    time: candle.time, 
+                    value: rsiValues[index]
+                }))
+            );
+            const generateLevelData = (levelValue) => {
+                if (userData.candle_stick_data.length === 0) return [];
+                const start = userData.candle_stick_data[0].time;
+                const end = userData.candle_stick_data[userData.candle_stick_data.length - 1].time;
+                return [{ time: start, value: levelValue }, { time: end, value: levelValue }];
+            };
+            
+            // Add overbought line at 70
+            const overboughtSeries = rsiChart.addLineSeries({
+                color: '#FF0000', // Red color for overbought
+                lineWidth: 1,
+            });
+            overboughtSeries.setData(generateLevelData(70));
+            
+            // Add oversold line at 30
+            const oversoldSeries = rsiChart.addLineSeries({
+                color: '#00FF00', // Green color for oversold
+                lineWidth: 1,
+            });
+            oversoldSeries.setData(generateLevelData(30));
+            
+            
+        }
+
+        return () => {
+            chart.remove();
+            rsiChart.remove();
+        }
+    }, [userData.candle_stick_data]);
+
+
+    return {
+        chartContainerRef,
+        rsiContainerRef,
+        userData,
+        getData,
+        setSearchState,
+        handleDropdownChange
+    }
+
+}
