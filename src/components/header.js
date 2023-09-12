d55560590414d08aba67823703e01e45871a5049
= <=>
2023-09-11 21:06:40 -0500

    update header and search

--- src/components/header.js
+++ src/components/header.js
@@ -14,14 +14,16 @@ const Header = () => {
   };
 
   return (
-    <div className={`flex justify-between items-center py-2 px-4 shadow-2xl bg-gradient-to-r ${theme === 'light' ? 'from-white to-gray-100' : 'from-gray-900 to-black'}`}>
+<div className={`flex justify-between items-center py-2 px-4 shadow-2xl bg-gradient-to-r ${theme === 'light' ? 'from-white to-gray-100' : 'from-gray-900 to-black'}`}>
       
       {/* Hamburger Menu */}
       <div onClick={toggleSidebarOpen} className={iconClass('')}>
         <FaBars className={theme === 'light' ? 'text-gray-800' : 'text-white'} />
-
       </div>
-
+    
+      {/* Desktop Search Component */}
+      <SearchInput isModal={false} className="flex-grow mx-4 md:mx-8 hidden md:block" />
+    
       <div className="flex items-center space-x-4">
         <div className="flex space-x-2 items-center">
           <div className={iconClass('')}>
@@ -34,18 +36,17 @@ const Header = () => {
             <FaCog />
           </div>
         </div>
-
+    
         <div className={`rounded-full p-1 transition-transform transform hover:scale-105 ${theme === 'light' ? 'bg-gradient-to-r from-blue-300 to-pink-400' : 'bg-gradient-to-r from-purple-600 to-indigo-500'} cursor-pointer`} onClick={toggleTheme}>
           {theme === 'light' ? <FaMoon className="text-dark-primary p-2 rounded-full bg-black" /> : <FaSun className="text-dark-primary p-2 rounded-full bg-yellow-500" />}
         </div>
-
-        <SearchInput isModal={false} />
       </div>
-
+    
+      {/* Mobile Search Icon */}
       <div className="md:hidden">
         <FaSearch className={iconClass('')} onClick={() => setIsSearchModalOpen(true)} />
       </div>
-
+    
       {isSearchModalOpen && (
         <div className={`fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 ${theme === 'light' ? 'bg-light-background bg-opacity-50' : 'bg-dark-primary bg-opacity-50'}`} onClick={() => setIsSearchModalOpen(false)}>
           <div onClick={(e) => e.stopPropagation()} className="p-4 bg-opacity-40 backdrop-blur-md border border-transparent focus:border focus:border-indigo-300 rounded-md shadow-2xl">
