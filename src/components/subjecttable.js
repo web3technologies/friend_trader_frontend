036fe0bba12ce0360a37d5804408b933846f7c67
= <=>
2023-10-04 20:22:36 -0500

    upodate table with styling and percentages

--- src/components/subjecttable.js
+++ src/components/subjecttable.js
@@ -2,12 +2,13 @@
 import React, { useState } from 'react';
 import { FaSort, FaStar, FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import { useTheme } from '../context/ThemeContext';
 
 import useFriendTechSubjectList from '../hooks/usefriendtechsubjectlist';
 
 
 function SubjectTable() {
-
+    const { theme } = useTheme();
     const navigate = useNavigate();
     const { subjects } = useFriendTechSubjectList();
     const [isFavorited, setIsFavorited] = useState(false);
@@ -44,13 +45,21 @@ function SubjectTable() {
         }).format(rounded);
     }
 
-    console.log(subjects)
+    function formatPercent(num) {
+        let rounded = Math.round(num * 10000) / 10000;
+        return new Intl.NumberFormat('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }).format(rounded);
+    }
 
     return (
         <div className="mt-5">
-            <h2 className="text-2xl font-bold mb-4">Top Subjects Overview!</h2>
-            <table className="min-w-full bg-white rounded shadow divide-y divide-gray-200">
-                <thead className="bg-gray-50">
+            <h2 className={`text-2xl font-bold mb-4 transform hover:-translate-y-1 hover:shadow-lg transition-transform duration-200 ${theme === 'light' ? 'text-gray-800' : 'text-white'} bg-gradient-to-r ${theme === 'light' ? 'from-blue-400 to-pink-500' : 'from-purple-700 to-indigo-600'}`}>
+                Top Subjects Overview!
+            </h2>
+            <table className={`min-w-full rounded shadow divide-y ${theme === 'light' ? 'bg-white' : 'bg-gray-900'}`}>
+                <thead className={`bg-gradient-to-r ${theme === 'light' ? 'from-white to-gray-100' : 'from-gray-900 to-black'}`}>
                     <tr>
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"></th>
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer" onClick={() => handleSort('name')}>
@@ -65,6 +74,12 @@ function SubjectTable() {
                         <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer" onClick={() => handleSort('price')}>
                             Price {sortField === 'price' && (sortOrder ? <FaChevronUp /> : <FaChevronDown />)}
                         </th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer" onClick={() => handleSort('price')}>
+                            24h % {sortField === 'price' && (sortOrder ? <FaChevronUp /> : <FaChevronDown />)}
+                        </th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer" onClick={() => handleSort('price')}>
+                            7d % {sortField === 'price' && (sortOrder ? <FaChevronUp /> : <FaChevronDown />)}
+                        </th>
                         <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer" onClick={() => handleSort('shares_supply')}>
                             Share Supply {sortField === 'shares_supply' && (sortOrder ? <FaChevronUp /> : <FaChevronDown />)}
                         </th>
@@ -73,9 +88,9 @@ function SubjectTable() {
                         </th>
                     </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
+                <tbody className={`${theme === 'light' ? 'bg-white' : 'bg-gray-900'} divide-y divide-gray-200`}>
                     {subjects.length > 1 ? subjects.map((subject, idx) => (
-                        <tr key={subject.twitter_username} className="hover:bg-gray-100 transition-colors duration-200">
+                        <tr key={subject.twitter_username} className={`${theme === 'light' ? 'hover:bg-gray-200 text-gray-900' : 'hover:bg-gray-800 text-white'} transition-colors duration-200`}>
                             <td className="px-6 py-4 text-left cursor-pointer" onClick={toggleFavorite}>
                                 <FaStar style={{ color: isFavorited ? 'gold' : 'transparent', strokeWidth: '5', stroke: 'black' }} />
                             </td>
@@ -88,7 +103,46 @@ function SubjectTable() {
                             </td>
                             <td className="px-6 py-4 text-left">{new Date(subject.last_trade_time * 1000).toLocaleString()}</td>
                             <td className="px-6 py-4 text-right">Ξ {formatNumber(subject.latest_price.price)}</td>
-                            <td className="px-6 py-4 text-right">{formatNumber(subject.shares_supply)}</td>
+                            <td className="px-6 py-4 text-right">
+                                <div className="flex items-center">
+                                    {subject.twenty_four_hour_percent_change > 0 ? 
+                                        <FaChevronUp className="text-green-500 mr-1" /> : 
+                                        subject.twenty_four_hour_percent_change < 0 ?
+                                            <FaChevronDown className="text-red-500 mr-1" /> :
+                                            null
+                                    }
+                                    <span className={
+                                        subject.twenty_four_hour_percent_change > 0 ? "text-green-500" :
+                                        subject.twenty_four_hour_percent_change < 0 ? "text-red-500" :
+                                        "text-gray-500"
+                                    }>
+                                        {subject.twenty_four_hour_percent_change !== 0 ? 
+                                            formatPercent(subject.twenty_four_hour_percent_change) + " %" : 
+                                            '—'}
+                                    </span>
+                                </div>
+                            </td>
+
+                            <td className="px-6 py-4 text-right">
+                                <div className="flex items-center">
+                                    {subject.seven_day_percent_change > 0 ? 
+                                        <FaChevronUp className="text-green-500 mr-1" /> : 
+                                        subject.seven_day_percent_change < 0 ?
+                                            <FaChevronDown className="text-red-500 mr-1" /> :
+                                            null
+                                    }
+                                    <span className={
+                                        subject.seven_day_percent_change > 0 ? "text-green-500" :
+                                        subject.seven_day_percent_change < 0 ? "text-red-500" :
+                                        "text-gray-500"
+                                    }>
+                                        {subject.seven_day_percent_change !== 0 ? 
+                                            formatPercent(subject.seven_day_percent_change) + " %" : 
+                                            '—'}
+                                    </span>
+                                </div>
+                            </td>
+                            <td className="px-6 py-4 text-right">{subject.shares_supply}</td>
                             <td className="px-6 py-4 text-right">Ξ {formatNumber(subject.latest_price.price * subject.shares_supply)}</td>
                         </tr>
                     )) : null}
